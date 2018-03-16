af295c542ce5ae99777d7dbb43439e70dfffe5fd
brankodencic <branko.dencic@quadro-tnation.com>
2018-03-16 22:26:02 +0100

    init-angular-tour-of-heroes

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+	title = 'Tour of Heroes';
+}
+
+// https://angular.io/tutorial/toh-pt3#masterdetail-components
